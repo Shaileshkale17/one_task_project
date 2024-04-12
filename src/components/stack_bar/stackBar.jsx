6db35ed94a13b00e36db48d42323ef60beadbe2f
Shailesh kale <shaileshkale87730@gmail.com>
2024-04-12 15:15:02 +0530

    stackber adding text and remove on image

--- src/components/stack_bar/stackBar.jsx
+++ src/components/stack_bar/stackBar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import stackBar from "../../assets/Property 1=Darkmode-Bar.svg";
+import arrow from "../../assets/Icons=Arrow-b.svg";
 const StackBar = () => {
   return (
-    <div>
-      <img src={stackBar} alt="" className="w-full" />
+    <div className="bg-gradient-to-r from-purple-100 via-pink-200 to-blue-200  py-2 text-center ">
+      <div className="flex flex-row justify-center items-center font-custom">
+        <p className="hidden md:block ">
+          This page is included in a free SaaS Website Kit.
+        </p>
+        <span className="mx-2 flex flex-row ">
+          View the complete Kit <img src={arrow} />
+        </span>
+      </div>
     </div>
   );
 };
